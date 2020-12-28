f5af8899c62e5fe592181a29d1d0cac0f34dfd3b
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2020-12-28 15:29:59 -0300

    Criação dos testes de validação Remoção do arquivo `.phpunit.result.cache` do versionamento

--- tests/Factories/ValidationFactoryTest.php
+++ tests/Factories/ValidationFactoryTest.php
@@ -3,7 +3,9 @@
 namespace CSWeb\Tests\Factories;
 
 use CSWeb\GlobalPayments\Factories\ValidationFactory;
+use Illuminate\Support\Arr;
 use Illuminate\Validation\Factory;
+use Illuminate\Validation\ValidationException;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -20,5 +22,35 @@ class ValidationFactoryTest extends TestCase
         $factory = ValidationFactory::make();
 
         $this->assertInstanceOf(Factory::class, $factory);
+    }
+
+    public function testDataValidation()
+    {
+        $this->expectExceptionMessage('The given data was invalid');
+        $this->expectException(ValidationException::class);
+
+        $factory = ValidationFactory::make();
+
+        $factory->validate([
+            'some_data' => null
+        ], [
+            'some_data' => 'required'
+        ]);
+    }
+
+    public function testValidationMessage()
+    {
+        $factory = ValidationFactory::make();
+        $message = null;
+
+        try {
+            $factory->validate([], [
+                'some_data' => 'required'
+            ]);
+        } catch (ValidationException $e) {
+            $message = Arr::get($e->errors(), 'some_data.0');
+        }
+
+        $this->assertEquals('O campo some data é obrigatório.', $message);
     }
 }
