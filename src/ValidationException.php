a38214560aab226d5a0000e9fc162fa02efcb2d8
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2020-12-28 15:31:43 -0300

    Inclusão do arquivo `ValidationException`

---
+++ src/ValidationException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CSWeb\GlobalPayments;
+
+use RuntimeException;
+
+/**
+ * Class ValidationException
+ *
+ * @author  Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+ * @version 1.0.0
+ * @package CSWeb\GlobalPayments
+ */
+class ValidationException extends RuntimeException
+{
+
+}
