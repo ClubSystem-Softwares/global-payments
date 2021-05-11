897ce3da251f7a0aa6bc5d508eb458ef804dfe4b
Matheus Lopes Santos <matheussantos@devsquad.com>
2021-05-10 22:04:25 -0300

    Inclusão de json de exemplo contendo o retorno da transação

--- src/Invoice.php
+++ src/Invoice.php
@@ -10,6 +10,23 @@ use Illuminate\Support\{Fluent};
  * @author  Matheus Lopes Santos <fale_com_lopez@hotmail.com>
  * @version 1.0.0
  * @package CSWeb\GlobalPayments
+ * @property $amount
+ * @property $currency
+ * @property $order
+ * @property $signature
+ * @property $merchantCode
+ * @property $terminal
+ * @property $response
+ * @property $authorisationCode
+ * @property $transactionType
+ * @property $securePayment
+ * @property $language
+ * @property $cardType
+ * @property $merchantData
+ * @property $cardCountry
+ * @property $nsu
+ * @property $cardBrand
+ * @property $processedPayMethod
  */
 class Invoice extends Fluent
 {
