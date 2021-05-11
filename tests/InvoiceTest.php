657d7fed1695da5186a065e23a3d3dabc9e4bcbe
Matheus Lopes Santos <matheussantos@devsquad.com>
2021-05-10 22:12:48 -0300

    Correção de testes

--- tests/InvoiceTest.php
+++ tests/InvoiceTest.php
@@ -17,43 +17,43 @@ class InvoiceTest extends TestCase
 {
     public function testInvoiceHydratation()
     {
-        $std = new stdClass();
+        $data = [
+            'amount'             => '1000',
+            'currency'           => '986',
+            'order'              => '6980eVlWfRri',
+            'signature'          => 'DB5EB7B8299854D1C2B48FDEA013715CD0CAA0D2F38844FFF0DE0AC914CAE25A',
+            'merchantCode'       => '012005733649001',
+            'terminal'           => '1',
+            'response'           => '0000',
+            'authorisationCode'  => '656141',
+            'transactionType'    => 'A',
+            'securePayment'      => '0',
+            'language'           => '9',
+            'cardType'           => 'C',
+            'merchantData'       => [],
+            'cardCountry'        => '724',
+            'nsu'                => '656141',
+            'cardBrand'          => '1',
+            'processedPayMethod' => '3',
+        ];
 
-        $std->Ds_Amount             = '1000';
-        $std->Ds_Currency           = '986';
-        $std->Ds_Order              = '6980eVlWfRri';
-        $std->Ds_Signature          = 'DB5EB7B8299854D1C2B48FDEA013715CD0CAA0D2F38844FFF0DE0AC914CAE25A';
-        $std->Ds_MerchantCode       = '012005733649001';
-        $std->Ds_Terminal           = '1';
-        $std->Ds_Response           = '0000';
-        $std->Ds_AuthorisationCode  = '656141';
-        $std->Ds_TransactionType    = 'A';
-        $std->Ds_SecurePayment      = '0';
-        $std->Ds_Language           = '9';
-        $std->Ds_Card_Type          = 'C';
-        $std->Ds_MerchantData       = new stdClass();
-        $std->Ds_Card_Country       = '724';
-        $std->Ds_Nsu                = '656141';
-        $std->Ds_Card_Brand         = '1';
-        $std->Ds_ProcessedPayMethod = '3';
+        $invoice = new Invoice($data);
 
-        $invoice = new Invoice($std);
-
-        $this->assertEquals($std->Ds_Amount, $invoice->amount);
-        $this->assertEquals($std->Ds_Currency, $invoice->currency);
-        $this->assertEquals($std->Ds_Order, $invoice->order);
-        $this->assertEquals($std->Ds_Signature, $invoice->signature);
-        $this->assertEquals($std->Ds_MerchantCode, $invoice->merchantCode);
-        $this->assertEquals($std->Ds_Terminal, $invoice->terminal);
-        $this->assertEquals($std->Ds_Response, $invoice->response);
-        $this->assertEquals($std->Ds_AuthorisationCode, $invoice->authorisationCode);
-        $this->assertEquals($std->Ds_TransactionType, $invoice->transactionType);
-        $this->assertEquals($std->Ds_SecurePayment, $invoice->securePayment);
-        $this->assertEquals($std->Ds_Language, $invoice->language);
-        $this->assertEquals($std->Ds_Card_Type, $invoice->cardType);
-        $this->assertEquals($std->Ds_Card_Country, $invoice->cardCountry);
-        $this->assertEquals($std->Ds_Nsu, $invoice->nsu);
-        $this->assertEquals($std->Ds_Card_Brand, $invoice->cardBrand);
-        $this->assertEquals($std->Ds_ProcessedPayMethod, $invoice->processedPayMethod);
+        $this->assertEquals($data['amount'], $invoice->amount);
+        $this->assertEquals($data['currency'], $invoice->currency);
+        $this->assertEquals($data['order'], $invoice->order);
+        $this->assertEquals($data['signature'], $invoice->signature);
+        $this->assertEquals($data['merchantCode'], $invoice->merchantCode);
+        $this->assertEquals($data['terminal'], $invoice->terminal);
+        $this->assertEquals($data['response'], $invoice->response);
+        $this->assertEquals($data['authorisationCode'], $invoice->authorisationCode);
+        $this->assertEquals($data['transactionType'], $invoice->transactionType);
+        $this->assertEquals($data['securePayment'], $invoice->securePayment);
+        $this->assertEquals($data['language'], $invoice->language);
+        $this->assertEquals($data['cardType'], $invoice->cardType);
+        $this->assertEquals($data['cardCountry'], $invoice->cardCountry);
+        $this->assertEquals($data['nsu'], $invoice->nsu);
+        $this->assertEquals($data['cardBrand'], $invoice->cardBrand);
+        $this->assertEquals($data['processedPayMethod'], $invoice->processedPayMethod);
     }
 }
